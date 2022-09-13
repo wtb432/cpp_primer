1ca9352a49b4ba4e33b102741075dbca0cfedda2
sjf <s1973853034@163.com>
2022-09-13 23:50:02 +0800

    first commit

---
+++ capter_12/12_7.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<memory>
+using namespace std;
+//using Ptr = shared_ptr<vector<int>> 
+auto _populate()
+{
+	vector<int> vec;
+	for(int i ;cout<<"Pls Enter:\n",cin>>i;vec.push_back(i));
+	return vec;
+}
+auto print(shared_ptr<vector<int>> vec) -> ostream&
+{
+	for(auto i:*vec) cout<<i<<" ";
+	return cout;
+}
+
+int main(){
+	shared_ptr<vector<int>> vec = make_shared<vector<int>>(_populate());
+	print(vec)<<endl;
+	return 0;
+}
