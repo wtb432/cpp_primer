15981d1fe28241dac1664865bf3596e599d8e58d
sjf <s1973853034@163.com>
2022-10-23 00:03:07 +0800

    refresh

--- capter_12/12_32.cpp
+++ capter_12/12_32.cpp
@@ -35,30 +35,42 @@ TextQuery::TextQuery(std::ifstream &is):file(new std::vector<std::string>){
 		}
 	}
 };
-
 class QueryResult{
-friend std::ostream& print(std::ostream& ,const QueryResult&);
-
 using line_no = std::vector<std::string>::size_type;
 public:
 QueryResult(std::string s,
 	    std::shared_ptr<std::set<line_no>> p,
 	    std::shared_ptr<std::vector<std::string>> f):
-	sought(s),lines(p),file(f){}
+	sought(s),lines(p),file(f){
+		_begin = lines->begin();
+		_end = lines->end();
+	}
+auto get_file() const -> std::shared_ptr<std::vector<std::string>>{
+	return file;
+}
+auto begin() const -> std::set<line_no>::iterator{
+	return _begin;
+}
+auto end() const ->std::set<line_no>::iterator{
+	return _end;
+}
+std::string get_word() const{
+	return sought;
+}
 private:
 	std::string sought;
 	std::shared_ptr<std::set<line_no>> lines;
 	std::shared_ptr<std::vector<std::string>> file;
-
+	std::set<line_no>::iterator _begin;
+	std::set<line_no>::iterator _end;
 };
-
 
 QueryResult TextQuery::query(const std::string &sought) const{
 	static std::shared_ptr<std::set<line_no>> nodata(new std::set<line_no>);
 	auto loc = wm.find(sought);
 	if(loc == wm.end())
 		return QueryResult(sought,nodata,file);
-	else
+	else 
 		return QueryResult(sought,loc->second,file);
 }
 
@@ -67,21 +79,26 @@ QueryResult TextQuery::query(const std::string &sought) const{
 std::string make_plural(std::size_t ctr,const std::string& word,const std::string& ending){
 	return (ctr>1)?word+ending:word;
 }
-std::ostream &print(std::ostream& os,const QueryResult &qr){
-	os<<qr.sought<<" occurs "<<qr.lines->size()<<" "
-	  <<make_plural(qr.lines->size(),"time","s")<<std::endl;
-	for(auto num:*qr.lines)
-		os<<"\t(line "<<num+1<<") "
-		  <<*(qr.file->begin()+num)<<std::endl;
-	return os;
 
+std::ostream &print(std::ostream& os,const QueryResult& qr){
+	os<<qr.get_word()<<" occurs "<<std::distance(qr.begin(),qr.end())<<" "
+	  <<make_plural(distance(qr.begin(),qr.end()),"times","s")<<std::endl;
+	auto p = qr.begin();
+	auto file = qr.get_file();
+	while(p!=qr.end()){
+		os<<"\t(line "<<*p+1<<") "
+	 	  <<*(file->begin()+*p)<<std::endl;
+		p++;
+	}
+	return os;
 }
+
 void runQueries(std::ifstream &infile){
 	TextQuery tq(infile);
 	while(true){
 		std::cout<<"enter word to look for ,or q to quit: ";
 		std::string s;
-		if(!(std::cin>>s)||s == "q") break;
+		if(!(std::cin>>s)||s=="q") break;
 		print(std::cout,tq.query(s))<<std::endl;
 	}
 }
@@ -90,3 +107,7 @@ int main(){
 	runQueries(ftr);
 	return 0;
 }
+
+
+
+
